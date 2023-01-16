2b461deadf831d025475032fcf0ccb789e8231a3
Dundaki <vigjoaopaulo@gmail.com>
2023-01-16 14:01:33 -0300

    registro nao esta sendo listado na tabela

--- src/main/java/com/api/parkingcontrol/controllers/ProdutosController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ProdutosController.java
@@ -3,6 +3,7 @@ package com.api.parkingcontrol.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.api.parkingcontrol.services.ProdutoService;
 
 @Controller
 @RestController
+@CrossOrigin(origins = "*")
 public class ProdutosController {
     
     @Autowired
